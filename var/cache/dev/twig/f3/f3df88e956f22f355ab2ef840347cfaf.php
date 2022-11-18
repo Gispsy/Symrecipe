99bca26b0f869221bfc721859c2d9b500856b642
Jarod <mordredbta3011@gmail.com>
2022-11-18 11:19:57 +0100

    First push

---
+++ var/cache/dev/twig/f3/f3df88e956f22f355ab2ef840347cfaf.php
@@ -0,0 +1,100 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* partials/_header.html.twig */
+class __TwigTemplate_6251ad532609eb7fa97b96c29b9f7320 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "partials/_header.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "partials/_header.html.twig"));
+
+        // line 1
+        echo "<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">
+  <div class=\"container-fluid\">
+    <a class=\"navbar-brand\" href=\"#\">Navbar</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarColor02\" aria-controls=\"navbarColor02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+      <span class=\"navbar-toggler-icon\"></span>
+    </button>
+    <div class=\"collapse navbar-collapse\" id=\"navbarColor02\">
+      <ul class=\"navbar-nav me-auto\">
+        <li class=\"nav-item\">
+          <a class=\"nav-link active\" href=\"#\">Home
+            <span class=\"visually-hidden\">(current)</span>
+          </a>
+        </li>
+    
+      </ul>
+    </div>
+  </div>
+</nav>";
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "partials/_header.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">
+  <div class=\"container-fluid\">
+    <a class=\"navbar-brand\" href=\"#\">Navbar</a>
+    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarColor02\" aria-controls=\"navbarColor02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+      <span class=\"navbar-toggler-icon\"></span>
+    </button>
+    <div class=\"collapse navbar-collapse\" id=\"navbarColor02\">
+      <ul class=\"navbar-nav me-auto\">
+        <li class=\"nav-item\">
+          <a class=\"nav-link active\" href=\"#\">Home
+            <span class=\"visually-hidden\">(current)</span>
+          </a>
+        </li>
+    
+      </ul>
+    </div>
+  </div>
+</nav>", "partials/_header.html.twig", "/home/stag9/Bureau/MS_DIW/BACK/SERVEUR/PHP/symfony/TestSymfony/templates/partials/_header.html.twig");
+    }
+}
